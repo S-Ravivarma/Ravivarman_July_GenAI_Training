dc52663d746ddbcb30f07bd9529b9164f021620c
Ravi varman.S <ravivarmanselvakumar@gmail.com>
2026-07-17 14:19:35 +0530

    Add files via upload

---
+++ Python_Task/result.py
@@ -0,0 +1,47 @@
+def analyze_result(name, roll, marks):
+    
+    total = sum(marks)
+    average = total / len(marks)
+
+   
+    if average >= 90:
+        grade = "A"
+    elif average >= 75:
+        grade = "B"
+    elif average >= 60:
+        grade = "C"
+    elif average >= 40:
+        grade = "D"
+    else:
+        grade = "Fail"
+
+
+    print(f"Student: {name} (Roll: {roll})")
+    print(f"Total: {total}")
+    print(f"Average: {average:.2f}")
+    print(f"Grade: {grade}")
+
+    
+    print("Subjects below 40:")
+    found = False
+
+    for index, mark in enumerate(marks):
+        if mark < 40:
+            print(f"Subject {index + 1}")
+            found = True
+
+    if not found:
+        print("None")
+
+
+
+name = input("Enter student name :")
+roll = int(input("Enter student roll number :"))
+marks = []
+
+for i in range(5):
+    mark = float(input(f"Enter Subject {i+1} Mark: "))
+    marks.append(mark)
+
+
+analyze_result(name, roll, marks)
